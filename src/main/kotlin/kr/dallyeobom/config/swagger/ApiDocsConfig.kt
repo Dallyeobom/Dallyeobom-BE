b6379ae8fb3c8b2477c031d191c0e9c1b73ddb5e
konempty <jameswebb@naver.com>
2025-05-28 22:49:45 +0900

    DAL-5 BE 배포 셋업

--- src/main/kotlin/kr/dallyeobom/config/swagger/ApiDocsConfig.kt
+++ src/main/kotlin/kr/dallyeobom/config/swagger/ApiDocsConfig.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders.AUTHORIZATION
@@ -14,6 +15,7 @@ class ApiDocsConfig {
     @Bean
     fun openAPI(): OpenAPI =
         OpenAPI()
+            .servers(mutableListOf(Server().apply { url = "/" }))
             .info(createApiInfo())
             .components(createSecurityComponents())
             .addSecurityItem(createSecurityRequirement())
