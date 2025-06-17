fcb8eb5f18f562bbfb8ab8ec5670c775b10d102e
yuhwanwoo <6514687@naver.com>
2025-06-17 23:34:10 +0900

    [DAL-17] yaml 분리

--- src/main/kotlin/kr/dallyeobom/util/MapUtils.kt
+++ src/main/kotlin/kr/dallyeobom/util/MapUtils.kt
@@ -3,4 +3,4 @@ package kr.dallyeobom.util
 import java.net.URLEncoder
 
 fun Map<String, String>.toFormUrlEncoded(): String =
-    this.entries.joinToString("&") { "${it.key}=${URLEncoder.encode(it.value, Charsets.UTF_8)}" }
+    entries.joinToString("&") { "${it.key}=${URLEncoder.encode(it.value, Charsets.UTF_8)}" }
