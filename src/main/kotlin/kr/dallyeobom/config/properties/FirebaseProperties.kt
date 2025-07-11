e100ba8c050bd713a5734afce03e19d5c9eccafb
konempty <jameswebb@naver.com>
2025-07-12 00:03:59 +0900

    DAL-47 프로퍼티명 변경

--- src/main/kotlin/kr/dallyeobom/config/properties/FirebaseProperties.kt
+++ src/main/kotlin/kr/dallyeobom/config/properties/FirebaseProperties.kt
@@ -2,7 +2,7 @@ package kr.dallyeobom.config.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties("firebase.fcm")
+@ConfigurationProperties("firebase")
 data class FirebaseProperties(
     val firebaseServiceAccountJson: String,
 )
